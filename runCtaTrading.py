d9441494c2f59e781ce2a8f5719c22a650eb76d6
ChetWang1993 <elwinwang1993@gmail.com>
2019-01-06 22:46:09 +0800

    create order log dit

--- runCtaTrading.py
+++ runCtaTrading.py
@@ -9,6 +9,9 @@ from datetime import datetime
 settingFileName = sys.argv[1]
 setting = json.load(open('conf/' + settingFileName))
 strat = testStrategy(setting)
+if not os.path.exists(setting['order_log_dir'].format(setting['symbol'])):
+    os.makedirs(setting['order_log_dir'].format(setting['symbol']))
+
 okApi = okApi(setting['apiKey'], setting['secretKey'], 
     setting['order_log_dir'].format(setting['symbol']) + setting['order_log_name'].format(datetime.now()))
 while(True):
