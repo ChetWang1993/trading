b535ccbd7f63231eb5f50397dc31f07b38813364
ChetWang1993 <elwinwang1993@gmail.com>
2019-01-06 22:40:27 +0800

    dy conf

--- strat/dtStrat.py
+++ strat/dtStrat.py
@@ -72,7 +72,7 @@ class testStrategy():
         if not os.path.exists(self.order_log_dir):
             os.makedirs(self.order_log_dir)
         self.okApi = okApi(self.apiKey, self.secretKey,
-            self.order_log_dir.format(self.symbol) + self.order_log_name.format(datetime.now()))
+            self.order_log_dir.format(self.symbol) + self.order_log_name.format(datetime.now().strftime('%Y%m%d')))
         # 载入历史数据，并采用回放计算的方式初始化策略数值
         self.initPrice()
         #self.putEvent()
