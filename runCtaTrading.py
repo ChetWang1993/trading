3134172c45564f9e5e2a885f3f46392adabb44be
ChetWang1993 <elwinwang1993@gmail.com>
2019-01-07 20:51:09 +0800

    add tick cb

--- runCtaTrading.py
+++ runCtaTrading.py
@@ -17,19 +17,21 @@ okApi = okApi(setting['apiKey'], setting['secretKey'],
     setting['order_log_dir'].format(setting['symbol']) + setting['order_log_name'].format(datetime.now()))
 while(True):
     try:
-    	tick = okApi.get_okex("/api/futures/v3/instruments/" + setting['okSymbol'] + "/ticker")
-        t = VtTickData()   
-        # 成交数据
-        t.lastPrice = tick['last']           # 最新成交价
-        t.volume = 0                 # 今天总成交量
-        t.openInterest = 0           # 持仓量
-        if not '.' in tick['timestamp']:
-            t.datetime = datetime.strptime(tick['timestamp'], '%Y-%m-%dT%H:%M:%SZ')
-        else:
-            t.datetime = datetime.strptime(tick['timestamp'], '%Y-%m-%dT%H:%M:%S.%fZ')
-        strat.onTick(t)
+    	tickCB(okApi.get_okex("/api/futures/v3/instruments/" + setting['okSymbol'] + "/ticker"))
     except Exception as e:
         print('[ERROR]: get tick {}'.format(e))
         time.sleep(1)
         continue
     time.sleep(1)
+
+def tickCB(tick):
+    t = VtTickData()   
+    # 成交数据
+    t.lastPrice = tick['last']           # 最新成交价
+    t.volume = 0                 # 今天总成交量
+    t.openInterest = 0           # 持仓量
+    if not '.' in tick['timestamp']:
+        t.datetime = datetime.strptime(tick['timestamp'], '%Y-%m-%dT%H:%M:%SZ')
+    else:
+        t.datetime = datetime.strptime(tick['timestamp'], '%Y-%m-%dT%H:%M:%S.%fZ')
+    strat.onTick(t)
